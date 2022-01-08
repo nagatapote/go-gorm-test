dd802d111aa8fb35e34cb7565989ca1add7cedc7
nagatapote <hiroaki29p@icloud.com>
2022-01-09 08:41:38 +0900

    add: insert

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
+	"go-todo-test/models"
 	"net/http"
 	"os"
 	"time"
@@ -11,30 +12,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func main() {
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		panic(err)
-	}
-	DBUser := os.Getenv("DB_USER")
-	DBPass := os.Getenv("DB_PASS")
-
-	db, err := sql.Open(
-		"postgres",
-		"host=localhost port=5432 dbname=sample_database user="+DBUser+" password="+DBPass+" sslmode=disable")
-
-	if err != nil && err.Error() != "" {
-		log.Fatal(err.Error())
-	}
-
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(10)
-	db.SetConnMaxLifetime(300 * time.Second)
-
-	fmt.Println("data base ok")
+	initDB()
+	insert()
 
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
@@ -44,4 +28,58 @@ func main() {
 	})
 
 	engine.Run(":3000")
+}
+
+func initDB() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	DBUser := os.Getenv("DB_USER")
+	DBPass := os.Getenv("DB_PASS")
+
+	dns := "host=localhost port=5432 dbname=sample_database user=" + DBUser + " password=" + DBPass + " sslmode=disable"
+
+	db, err := sql.Open("postgres", dns)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// connection pool settings
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxLifetime(300 * time.Second)
+
+	// global connection setting
+	boil.SetDB(db)
+	boil.DebugMode = true
+
+	fmt.Println("data base ok")
+}
+
+func insert() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	DBUser := os.Getenv("DB_USER")
+	DBPass := os.Getenv("DB_PASS")
+
+	dns := "host=localhost port=5432 dbname=sample_database user=" + DBUser + " password=" + DBPass + " sslmode=disable"
+
+	db, err := sql.Open("postgres", dns)
+
+	if err != nil {
+		panic(err)
+	}
+
+	user := models.User{
+		Email:          null.StringFrom("test@example.com"),
+		PasswordDigest: null.StringFrom("digested-password"),
+	}
+	fmt.Printf("before user = %+v\n", user)
+	user.Insert(context.Background(), db, boil.Infer())
+	fmt.Printf("after user = %+v\n", user)
 }
